e2d01cae0d989231fc61931db48083dba6e89443
AliMohsin2004 <92257564+AliMohsin2004@users.noreply.github.com>
2025-09-02 00:41:07 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Inputfields from "./components/Inputfields";
 import Button from "./components/Button";
+import { FaEnvelope, FaLock } from "react-icons/fa";
 
 function App() {
   return (
@@ -12,14 +13,25 @@ function App() {
         <p className="desc font-semibold text-md gap-1 p-4 mt-2 lowercase text-[#84C7AE] text-center font-Roboto">
           Create an account to enjoy all the services without any ads for free!
         </p>
-        <div className="flex flex-col items-ccenter mt-1.5">
-          <Inputfields type="email" placeholder="Email Address..." />
-          <Inputfields type="password" placeholder="Enter Password..." />
+
+        <div className="flex flex-col items-center mt-2">
+          <Inputfields
+            type="email"
+            placeholder="Email Address..."
+            icon={<FaEnvelope />}
+          />
+          <Inputfields
+            type="password"
+            placeholder="Enter Password..."
+            icon={<FaLock />}
+          />
         </div>
+
         <div>
           <Button />
         </div>
-        <p className="flex justify-center text-[#84C7AE] mt-2 font-Roboto font-ligh gap-1">
+
+        <p className="flex justify-center text-[#84C7AE] mt-2 font-Roboto font-light gap-1">
           already have an account?
           <span className="font-semibold text-[#22503F] underline cursor-pointer">
             SignUp
