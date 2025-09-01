66411fc65b4cbfa2d7e10a96ac5874795cc1d863
AliMohsin2004 <92257564+AliMohsin2004@users.noreply.github.com>
2025-09-02 00:39:47 +0500

    Add files via upload

--- Inputfields.jsx
+++ Inputfields.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import {FaEnvelope,FaLock} from "react-icons/fa";
+import { FaEnvelope, FaLock } from "react-icons/fa";
 
-function inputfields({ type = "text", placeholder }) {
+// Reusable Input Field Component
+function Inputfields({ type = "text", placeholder, icon }) {
   return (
-    <div>
-    <input
-      type={type}
-      placeholder={placeholder}
-      className="w-80 h-12 border-2 rounded-xl mt-2 font-light font-Roboto text-sm px-3 bg-white"
-    /> 
-    </div>
+    <div className="flex items-center w-80 h-12 border-2 rounded-xl mt-2   bg-white px-3">
     
+      {/* Input */}
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="flex-1 h-full outline-none px-2 font-light font-Roboto text-sm bg-transparent"
+      />
+        {/* Icon */}
+      <span className="text-gray-500 text-sm flex justify-between">{icon}</span>
+    </div>
   );
 }
 
-export default inputfields;
+export default Inputfields;
